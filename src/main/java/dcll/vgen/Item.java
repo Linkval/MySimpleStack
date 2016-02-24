dc18ea67a8e988b3547a022b7238bcfa0ac35bf2
Linkval <genin.valentin@gmail.com>
2016-02-24 11:08:31 +0100

    commit implementation SimpleStack

---
+++ src/main/java/dcll/vgen/Item.java
@@ -0,0 +1,21 @@
+package dcll.vgen;
+
+/**
+ * Created by 21000539 on 24/02/2016.
+ */
+public class Item {
+
+    int val;
+
+    public Item(int val) {
+        this.val = val;
+    }
+
+    public int getVal() {
+        return val;
+    }
+
+    public void setVal(int val) {
+        this.val = val;
+    }
+}
---
+++ src/test/java/dcll/vgen/ItemTest.java
@@ -0,0 +1,30 @@
+package dcll.vgen;
+
+import junit.framework.TestCase;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class ItemTest extends TestCase {
+    Item item;
+    @Before
+    public void setUp() throws Exception {
+        item = new Item(2);
+    }
+
+    public void testConstruct() throws Exception {
+        assertEquals(2,item.getVal());
+    }
+
+    @Test
+    public void testGetVal() throws Exception {
+        assertEquals(2,item.getVal());
+    }
+
+    @Test
+    public void testSetVal() throws Exception {
+        item.setVal(3);
+        assertEquals(3,item.getVal());
+    }
+}
